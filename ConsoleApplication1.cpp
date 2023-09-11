fe9f5a7383df599a87e579432e3897cf959bff1b
Monobrov472 <144647756+Monobrov472@users.noreply.github.com>
2023-09-11 08:07:41 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream> //i/o library
+using namespace std; //instead od prefix std::
+
+int main() //header
+{ //begin
+
+	float a, res; //declaration
+	//input
+	cout << "a = ";
+	cin >> a;
+	//calculation
+	res = a + 10;
+	//output
+	cout << "a+10 =" << res << endl;
+	system("pause");
+	return 0; //return to OS
+} //end
